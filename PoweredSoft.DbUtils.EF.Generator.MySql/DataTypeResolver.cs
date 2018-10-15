3d7c9ff411a80e6a87f875d9e70cfee4f6a42000
David Lebee <david@poweredsoft.com>
2018-10-15 18:36:37 -0500

    data type resolver is started for mysql.

---
+++ PoweredSoft.DbUtils.EF.Generator.MySql/DataTypeResolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PoweredSoft.DbUtils.EF.Generator.Core;
+using PoweredSoft.DbUtils.Schema.Core;
+
+namespace PoweredSoft.DbUtils.EF.Generator.MySql
+{
+
+    public class DataTypeResolver : IDataTypeResolver
+    {
+        public bool IsFixLength(IHasDataType meta)
+        {
+            return meta.DataType == "char";
+        }
+
+        public bool IsString(IHasDataType meta)
+        {
+            return meta.DataType == "varchar" || meta.DataType == "char" || meta.DataType == "text";
+        }
+
+        public bool IsUnicode(IHasDataType meta) => true;
+        public bool NeedFluentPrecisionSpecification(IHasDataType meta) => true;
+
+        public Type ResolveType(IHasDataType meta)
+        {
+            if (meta.DataType == "bigint" && meta.NumericPrecision == 20)
+                return typeof(ulong);
+            else if (meta.DataType == "bigint")
+                return typeof(long);
+
+            return typeof(string);
+        }
+    }
+}
