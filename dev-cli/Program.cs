95cf7790ab717f1184eaf5e6a8011375b93545b3
David Lebee <david@poweredsoft.com>
2018-10-24 13:39:44 -0500

    read me.

--- dev-cli/Program.cs
+++ dev-cli/Program.cs
@@ -1,5 +1,8 @@
-﻿using PoweredSoft.DbUtils.EF.Generator.EFCore.MySql;
+﻿
+using PoweredSoft.DbUtils.EF.Generator.EFCore.SqlServer;
+using PoweredSoft.DbUtils.Schema.SqlServer.Extensions;
 using System;
+using System.Linq;
 
 namespace dev_cli
 {
@@ -14,8 +17,8 @@ namespace dev_cli
 
             var gen = new DatabaseGenerator();
             gen.InitializeOptionsWithDefault();
-            //gen.Options.ConnectionString = "Server=ps-sql.dev;Database=Acme;user id=acme;password=-acmepw2016-";
-            gen.Options.ConnectionString = "server=192.168.100.154;uid=dlebee;pwd=-pssql2016-;database=Acme";
+            gen.Options.ConnectionString = "Server=ps-sql.dev;Database=Acme;user id=acme;password=-acmepw2016-";
+            //gen.Options.ConnectionString = "server=192.168.100.154;uid=dlebee;pwd=-pssql2016-;database=Acme";
             gen.Options.OutputDir = "C:\\test\\output";
             gen.Options.ContextName = "AcmeContext";
             gen.Options.OutputSingleFileName = "All.generated.cs";
@@ -24,7 +27,13 @@ namespace dev_cli
             gen.Options.ConnectionStringName = "Acme";
             gen.Options.GenerateContextSequenceMethods = true;
             gen.Options.AddConnectionStringOnGenerate = true;
-            gen.Generate();;
+            //gen.Generate();;
+
+            var schema = gen.CreateSchema();
+            schema.ConnectionString = gen.Options.ConnectionString;
+            schema.LoadSchema();
+
+            var firstSchema = schema.Tables.First().GetTableSchema();
         }
     }
 }
