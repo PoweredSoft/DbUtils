c630a97129d77df336dca64ebbfef1232632d7e9
David Lebee <david@poweredsoft.com>
2018-10-24 13:12:20 -0500

    added extension method to get table schema.

---
+++ PoweredSoft.DbUtils.Schema.SqlServer/Extensions/TableExtensions.cs
@@ -0,0 +1,12 @@
+﻿using PoweredSoft.DbUtils.Schema.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoweredSoft.DbUtils.Schema.SqlServer.Extensions
+{
+    public static class TableExtensions
+    {
+        public static string GetTableSchema(this ITable table) => ((Table)table).Schema;
+    }
+}
